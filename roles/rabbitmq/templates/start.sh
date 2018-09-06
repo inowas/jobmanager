167ac170389ffd1345c669c6c504da847ba6373d
jakobblume <j.blume@polylith.de>
2018-09-06 15:16:00 +0200

    added legacy user to rabbotmq startscript

--- roles/rabbitmq/templates/start.sh
+++ roles/rabbitmq/templates/start.sh
@@ -3,11 +3,20 @@
 ( sleep 10 ; \
 
 rabbitmqctl add_user admin {{ admin_user_password }}
-rabbitmqctl add_user worker {{ worker_user_password }}
-rabbitmqctl add_user backend {{ backend_user_password }}
+rabbitmqctl set_user_tags admin administrator
 rabbitmqctl set_permissions -p / admin ".*" ".*" ".*"
+
+rabbitmqctl add_user worker {{ worker_user_password }}
 rabbitmqctl set_permissions -p / worker ".*" ".*" ".*"
+
+rabbitmqctl add_user backend {{ backend_user_password }}
 rabbitmqctl set_permissions -p / backend ".*" ".*" ".*"
+
+# TODO: Remove this, its there for migrations reasons
+rabbitmqctl add_user inowas_user inowas_password
+rabbitmqctl add_vhost inowas_host
+rabbitmqctl set_permissions -p inowas_host worker ".*" ".*" ".*"
+rabbitmqctl set_user_tags inowas_user administrator
 
 ) &
 rabbitmq-server $@
