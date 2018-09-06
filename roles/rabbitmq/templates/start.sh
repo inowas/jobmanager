de03aef27b2103f59629556ebdfacaf2f8df9066
jakobblume <j.blume@polylith.de>
2018-09-06 10:42:38 +0200

    added rabbitmq user for backend and workers

--- roles/rabbitmq/templates/start.sh
+++ roles/rabbitmq/templates/start.sh
@@ -3,8 +3,11 @@
 ( sleep 10 ; \
 
 rabbitmqctl add_user admin {{ admin_user_password }}
-rabbitmqctl set_user_tags admin administrator
+rabbitmqctl add_user worker {{ worker_user_password }}
+rabbitmqctl add_user backend {{ backend_user_password }}
 rabbitmqctl set_permissions -p / admin ".*" ".*" ".*"
+rabbitmqctl set_permissions -p / worker ".*" ".*" ".*"
+rabbitmqctl set_permissions -p / backend ".*" ".*" ".*"
 
 ) &
 rabbitmq-server $@
